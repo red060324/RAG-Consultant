ccada9833f95454b7247704fc19f2e64b049fb24
Red <148629701+red060324@users.noreply.github.com>
2025-11-03 21:16:25 +0800

    Initial commit

---
+++ src/main/java/com/red/ragconsultant/RagConsultantApplication.java
@@ -0,0 +1,13 @@
+package com.red.ragconsultant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagConsultantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagConsultantApplication.class, args);
+    }
+
+}
