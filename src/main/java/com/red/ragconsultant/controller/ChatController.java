49224d38884b61cb623a64eea8863cef64d0fa2e
Red <148629701+red060324@users.noreply.github.com>
2025-11-03 21:44:50 +0800

    初始化项目，创建示例 chat 接口

---
+++ src/main/java/com/red/ragconsultant/controller/ChatController.java
@@ -0,0 +1,25 @@
+package com.red.ragconsultant.controller;
+
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author red
+ * @date 2025/11/3
+ * @description
+ */
+@RestController
+public class ChatController {
+
+    @Autowired
+    private OpenAiChatModel model;
+
+    @RequestMapping("/chat")
+    public String chat(String message) {
+        String result = model.chat(message);
+        return result;
+    }
+
+}
