ff11fd12c696bc6b5a9e70bd9189a0225e098803
Red <148629701+red060324@users.noreply.github.com>
2025-11-11 14:43:42 +0800

    更新 AI 助手名称为“小蓝盒盒娘”，并添加系统消息配置

--- src/main/java/com/red/ragconsultant/aiservice/ConsultantService.java
+++ src/main/java/com/red/ragconsultant/aiservice/ConsultantService.java
@@ -1,5 +1,6 @@
 package com.red.ragconsultant.aiservice;
 
+import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.spring.AiService;
 import reactor.core.publisher.Flux;
 
@@ -11,5 +12,7 @@ import reactor.core.publisher.Flux;
 @AiService
 public interface ConsultantService {
 //    public String chat(String message);
+//    @SystemMessage("你是一个游戏论坛助手，小蓝盒盒娘，你需要帮助用户解决游戏相关的问题，并提供有用的信息。")
+    @SystemMessage(fromResource = "system.txt")
     public Flux<String> chat(String message);
 }
