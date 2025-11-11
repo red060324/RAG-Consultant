7e0f74228d15ad3ea329487926b3182508cf6f02
Red <148629701+red060324@users.noreply.github.com>
2025-11-11 20:54:50 +0800

    添加文档分割器支持以增强文档处理能力

--- src/main/java/com/red/ragconsultant/config/CommonConfig.java
+++ src/main/java/com/red/ragconsultant/config/CommonConfig.java
@@ -3,8 +3,10 @@ package com.red.ragconsultant.config;
 import com.red.ragconsultant.aiservice.ConsultantService;
 import com.red.ragconsultant.repository.RedisChatMemoryStore;
 import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentSplitter;
 import dev.langchain4j.data.document.loader.ClassPathDocumentLoader;
 import dev.langchain4j.data.document.parser.apache.pdfbox.ApachePdfBoxDocumentParser;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
@@ -74,8 +76,13 @@ public class CommonConfig {
         // 构建向量数据库操作对象
         InMemoryEmbeddingStore store = new InMemoryEmbeddingStore();
         // 构建一个 EmbeddingStoreIngerstor 对象，完成对文档数据切割、向量化并存储到向量数据库中
+
+        // 构建文档分割器对象
+        DocumentSplitter ds = DocumentSplitters.recursive(1000, 100);
+
         EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
                 .embeddingStore(store)
+                .documentSplitter(ds)
                 .build();
         ingestor.ingest(documents);
         return store;
